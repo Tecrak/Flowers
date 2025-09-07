5757673526061db4b179e8c27fe198d6573a4511
Tecrak <87039557+Tecrak@users.noreply.github.com>
2025-09-07 11:56:01 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {Header} from '../components/header'
 import {MainPage} from '../components/mainPage'
 import {CartPage} from '../components/cartPage'
@@ -11,11 +11,9 @@ export function App() {
 
   return (
     <Router>
-      {/* Header тепер має доступ до сортування */}
       <Header onSortChange={setSortOption} />
       <Routes>
-        {/* Передаємо sortOption у MainPage */}
-        <Route path="/" element={<MainPage sortOption={sortOption} />} />
+        <Route path='/' element={<MainPage sortOption={sortOption} />} />
         <Route path='/components/cartPage.tsx' element={<CartPage />} />
         <Route path='/components/orderStory.tsx' element={<OrderStory />} />
       </Routes>
