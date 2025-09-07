e3af07c1298030e4364f15b1293c7c2a8c2ccb82
Tecrak <87039557+Tecrak@users.noreply.github.com>
2025-09-07 20:33:30 +0000

    commit

--- components/header.tsx
+++ components/header.tsx
@@ -14,9 +14,6 @@ export function Header({ onSortChange }: HeaderProps) {
           <Link to='/cart'>Shopping cart</Link>
           <Link to='/orders'>Order history</Link>
         </div>
-        <div className="logoImg">
-          <img src="https://cdn-icons-png.flaticon.com/512/616/616408.png" alt="flower logo" />
-        </div>
         <div className="rightPart">
           <div className="sortingBox">
             <span>Sort by:</span>
